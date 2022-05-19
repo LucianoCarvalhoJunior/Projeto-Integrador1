6cea2891627c5ce12d0383c607965d177da9a850
Projeto <projetointegradorr3@gmail.com>
2022-05-19 11:55:32 -0300

    Env

---
+++ EnvioDeEmail/Envio.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+const { text } = require("stream/consumers");
+
+const user = "jsteste93@gmail.com"
+const pass = "mpvs140403"
+
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {user, pass}
+})
+
+transporter.sendMail({
+    from: "Teste <jsteste93@gmail.com>",
+    to: "paulomasters.pro@gmail.com",
+    subject: "Curriculo",
+    text: "Deu Certo!"
+}).then(message => {
+    console.log(message);
+}).catch(err => {
+    console.log(err);
+})
